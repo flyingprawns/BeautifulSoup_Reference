a919f1c608748ed9763d883b8682dfedbe8d58d9
Qijie Zhu <qzhu9@ucsc.edu>
2021-10-30 23:20:06 -0700

    added live website scraping

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # ---------------- OPEN AND PARSE HTML FILE ------------------ #
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 
-with open("website.html", encoding="utf8") as file:
-    contents = file.read()
-
-soup = BeautifulSoup(contents, "html.parser")
+# with open("website.html", encoding="utf8") as file:
+#     contents = file.read()
+#
+# soup = BeautifulSoup(contents, "html.parser")
 
 # ---------------- PRINT WEBPAGE ------------------ #
 # print(soup.prettify())  # Print out indented version of entire webpage
@@ -23,11 +23,34 @@ soup = BeautifulSoup(contents, "html.parser")
 # print(section_heading)
 
 # ---------------- SEARCH FOR SPECIFIC TAG USING select_one()/select() ------------------ #
-company_url = soup.select_one(selector="p a")  # Searches for an "a" element inside a "p" element
-print(company_url)
+# company_url = soup.select_one(selector="p a")  # Searches for an "a" element inside a "p" element
+# print(company_url)
+#
+# name = soup.select_one(selector="#name")  # Searches for an id called "name"
+# print(name)
+#
+# headings = soup.select(selector=".heading")  # Searches for a class called "heading"
+# print(headings)
 
-name = soup.select_one(selector="#name")  # Searches for an id called "name"
-print(name)
 
-headings = soup.select(selector=".heading")  # Searches for a class called "heading"
-print(headings)
+# --------------------------------------------------------------- #
+# ---------------- PARSING FROM A LIVE WEBSITE ------------------ #
+# --------------------------------------------------------------- #
+
+# ---------------- OPEN AND PARSE WEBPAGE ------------------ #
+from bs4 import BeautifulSoup
+
+import requests
+
+response = requests.get("https://news.ycombinator.com/")
+response.raise_for_status()
+
+web_page = response.text
+
+soup = BeautifulSoup(web_page, "html.parser")
+
+# ---------------- PRINT TITLE AND UPVOTES OF FIRST ARTICLE ------------------ #
+article_tag = soup.find(name="a", class_="titlelink")
+article_upvote = soup.find(name="span", class_="score")
+print(article_tag.getText())
+print(article_upvote.getText())
